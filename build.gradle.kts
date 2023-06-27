4e5758b4b7a305a6b7859a7dd2e1bd4caf4efc6a
yosephog <thorfry@yahoo.com>
2023-06-27 10:17:26 +0200

    set up for project

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
