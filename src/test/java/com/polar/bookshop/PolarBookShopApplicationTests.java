dcdce70e381c0b1f2cc7b81ccaf18bb53ed4e96f
yosephog <thorfry@yahoo.com>
2023-07-05 11:24:01 +0200

    tests

--- src/test/java/com/polar/bookshop/PolarBookShopApplicationTests.java
+++ src/test/java/com/polar/bookshop/PolarBookShopApplicationTests.java
@@ -1,13 +1,32 @@
 package com.polar.bookshop;
 
+import com.polar.bookshop.domain.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient;
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PolarBookShopApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void whenPostRequestThenBookCreated() {
+		var expectedBook = new Book(1L,"1231231231", "Title", "Author", 9.90, null, null, 1);
+
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(expectedBook)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(actualBook -> {
+					assertThat(actualBook).isNotNull();
+					assertThat(actualBook.isbn())
+							.isEqualTo(expectedBook.isbn());
+				});
 	}
 
 }
